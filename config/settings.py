9df562107e547a1e9a43dfb9164d6730119b35e4
michaekong <michaelndekebai@gmail.com>
2025-12-06 20:06:50 +0100

    allow host

--- config/settings.py
+++ config/settings.py
@@ -18,9 +18,11 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "camsecureplot.reimca-app.com",
+   
+    "memocloudbackend.onrender.com",
     "frontcamsec.vercel.app",
     "127.0.0.1",
+    "memocloudfront.vercel.app",
     "camsecplots.netlify.app",
 ]
 
