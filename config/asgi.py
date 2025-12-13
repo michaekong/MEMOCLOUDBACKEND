edd9efa716497b6a69e61103c778ba762dab99b8
michaekong <michaelndekebai@gmail.com>
2025-12-13 04:43:32 +0100

    deploy on reimca

--- config/asgi.py
+++ config/asgi.py
@@ -1,15 +1,14 @@
 import os
 import django
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()  # Important to call before creating the application
+django.setup()  # 👈 important avant d'importer routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    # Remove the websocket entry if not needed
+ 
 })
-
-# If you need to handle WebSocket connections later, add them here.
