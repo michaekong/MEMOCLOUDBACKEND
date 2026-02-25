c5958ca7bab4b77f935d585fcca92d01c0094921
macdallas <roylexstephane@gmail.com>
2026-02-25 02:18:23 +0100

    Fake initial on entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,21 @@
 #!/bin/sh
 
-# -------------------------------
-# Entrypoint Django Production
-# -------------------------------
-
 # set -e  # Stop script on first error
 
-echo "---------------------------------"
-echo "Applying Django migrations (fake-initial)..."
-echo "---------------------------------"
-# --fake-initial : ignore les tables déjà créées
-python manage.py migrate --fake-initial
+echo "Cleaning old migrations..."
+find . -path "*/migrations/*.py" -not -name "__init__.py" -print -delete
+find . -path "*/migrations/*.pyc" -print -delete
 
-echo "---------------------------------"
+echo "Creating migrations..."
+python manage.py makemigrations
+
+echo "Applying migrations..."
+python manage.py migrate
+
 echo "Collecting static files..."
-echo "---------------------------------"
 python manage.py collectstatic --noinput
-
-# Vérifier le contenu du dossier static (optionnel)
 STATIC_DIR=$(python -c "from config.settings import STATIC_ROOT; print(STATIC_ROOT)")
-if [ -d "$STATIC_DIR" ]; then
-    echo "Static files in $STATIC_DIR:"
-    ls -l "$STATIC_DIR"
-fi
+[ -d "$STATIC_DIR" ] && ls -l "$STATIC_DIR"
 
-echo "---------------------------------"
 echo "Starting Django server..."
-echo "---------------------------------"
-# Utiliser exec pour remplacer le shell et ne pas bloquer le container
 exec python3 manage.py runserver 0.0.0.0:8000
