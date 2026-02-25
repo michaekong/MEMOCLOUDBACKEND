ec598bb7dc7ec942b26d8f8d7cb9f722576a2a4c
macdallas <roylexstephane@gmail.com>
2026-02-25 03:01:35 +0100

    Correct bug into migrations files

--- users/migrations/0003_initial.py
+++ users/migrations/0003_initial.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0002_initial'),
+        ('users', '0002_initial'), 
         ('universites', '0001_initial'),
     ]
 
