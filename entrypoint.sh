390d4f3c73a943cc459d331d072318f7e5508197
macdallas <roylexstephane@gmail.com>
2026-02-25 02:58:23 +0100

    remove makemigrations into entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
 
+# -------------------------------
+# Entrypoint Django Production
+# -------------------------------
+
 # set -e  # Stop script on first error
 
-echo "Cleaning old migrations..."
-find . -path "*/migrations/*.py" -not -name "__init__.py" -print -delete
-find . -path "*/migrations/*.pyc" -print -delete
+echo "---------------------------------"
+echo "Applying Django migrations (fake-initial)..."
+echo "---------------------------------"
+# --fake-initial : ignore les tables déjà créées
+python manage.py migrate --fake-initial
 
-echo "Creating migrations..."
-python manage.py makemigrations
-
-echo "Applying migrations..."
-python manage.py migrate
-
+echo "---------------------------------"
 echo "Collecting static files..."
+echo "---------------------------------"
 python manage.py collectstatic --noinput
-STATIC_DIR=$(python -c "from config.settings import STATIC_ROOT; print(STATIC_ROOT)")
-[ -d "$STATIC_DIR" ] && ls -l "$STATIC_DIR"
 
+# Vérifier le contenu du dossier static (optionnel)
+STATIC_DIR=$(python -c "from config.settings import STATIC_ROOT; print(STATIC_ROOT)")
+if [ -d "$STATIC_DIR" ]; then
+    echo "Static files in $STATIC_DIR:"
+    ls -l "$STATIC_DIR"
+fi
+
+echo "---------------------------------"
 echo "Starting Django server..."
+echo "---------------------------------"
+# Utiliser exec pour remplacer le shell et ne pas bloquer le container
 exec python3 manage.py runserver 0.0.0.0:8000
